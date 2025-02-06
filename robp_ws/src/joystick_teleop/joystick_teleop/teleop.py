dacc885044f07c99e655878f35819fb6130cd8ec
Francisco <franmira2003@gmail.com>
2025-02-06 22:55:08 +0100

    corrected teleop (joy 2 twist) and twist2duty nodes

--- robp_ws/src/joystick_teleop/joystick_teleop/teleop.py
+++ robp_ws/src/joystick_teleop/joystick_teleop/teleop.py
@@ -6,35 +6,48 @@ from sensor_msgs.msg import Joy
 from std_msgs.msg import Float32MultiArray, Header
 from robp_interfaces.msg import DutyCycles
 from geometry_msgs.msg import Twist
+from math import atan2, sin, sqrt, cos
 
 
 class F710Teleop(Node):
     def __init__(self):
         super().__init__('f710_teleop')
         self.subscription = self.create_subscription(Joy, '/joy', self.joy_callback, 10)
-        self.publisher = self.create_publisher(Twist, '/desired_twist', 10)
+        self.publisher = self.create_publisher(Twist, '/cmd_vel', 10)
 
         # Define joystick mappings (adjust based on testing)    
-        self.axis_linear = 7  # Left stick up/down
-        self.axis_angular = 6  # Left stick left/right
-        self.max_linear = 1.0 # maximum linear velocity
-        self.max_angular = 0.5 # maximum angular velocity
+        self.axis_straight = 7  # Left stick up/down
+        self.axis_sideways = 6  # Left stick left/right
+        self.axis_rotate = 2  # CHANGE LATER ON
+        self.max_linear = 0.1 # maximum linear velocity
+        self.max_angular = 0.05 # maximum angular velocity
 
     def joy_callback(self, msg):
         # Print joystick values
-        print(f'Up/down (button {self.axis_linear}): {msg.axes[self.axis_linear]}')
-        print(f'Left/right (button {self.axis_angular}): {msg.axes[self.axis_angular]}')
+        print(f'Up/down (button {self.axis_straight}): {msg.axes[self.axis_straight]}')
+        print(f'Left/right (button {self.axis_sideways}): {msg.axes[self.axis_sideways]}')
 
         # Publish twist commands
-        msg.header = Header()
-        msg.header.stamp = self.get_clock().now().to_msg()
+        twist_msg = Twist()
 
-        """
-        msg.linear.x = linear
-        msg.linear.y = 0.0
-        msg.angular.z = angular
-        """
-        self.publisher.publish(msg)
+        #theta = atan2(msg.axes[self.axis_straight], msg.axes[self.axis_sideways])
+        #vx = sin(theta)*sqrt(msg.axes[self.axis_straight]**2 + msg.axes[self.axis_sideways]**2)*(1/sqrt(2))*self.max_linear
+        #vx = cos(theta)*sqrt(msg.axes[self.axis_straight]**2 + msg.axes[self.axis_sideways]**2)*(1/sqrt(2))*self.max_linear
+
+        twist_msg.linear.x = min(max(msg.axes[self.axis_straight] * self.max_linear, -self.max_linear),self.max_linear)
+        twist_msg.linear.y = 0.0
+
+        if msg.buttons[self.axis_rotate] != 0:
+            twist_msg.angular.z = self.max_angular
+            twist_msg.linear.x = 0.0
+            twist_msg.linear.y = 0.0
+        else:
+            twist_msg.angular.z = 0.0
+
+        print(f'Linear Velocity (x,y): {twist_msg.linear.x} and {twist_msg.linear.y}')
+        print(f'Angular Velocity: {twist_msg.angular.z}')
+
+        self.publisher.publish(twist_msg)
 
 def main(args=None):
     rclpy.init(args=args)
--- robp_ws/src/joystick_teleop/joystick_teleop/twist2duty.py
+++ robp_ws/src/joystick_teleop/joystick_teleop/twist2duty.py
@@ -7,48 +7,52 @@ from std_msgs.msg import Float32MultiArray, Header
 from robp_interfaces.msg import DutyCycles
 from geometry_msgs.msg import Twist
 
-class F710Teleop(Node):
+class Twist2Duty(Node):
     def __init__(self):
-        super().__init__('f710_teleop')
-        self.subscription = self.create_subscription(Twist, '/desired_twist', 10)
+        super().__init__('twist2duty')
+        self.subscription = self.create_subscription(Twist, '/cmd_vel', self.twist2duty_callback, 10)
         self.publisher = self.create_publisher(DutyCycles, '/motor/duty_cycles', 10)
 
-        # Define joystick mappings (adjust based on testing)
-        self.axis_linear = 7  # Left stick up/down
-        self.axis_angular = 6  # Left stick left/right
-        self.scale_linear = 1.0
-        self.scale_angular = 0.5
+        # Define joystick mappings (adjust based on testing)    
+        self.axis_straight = 7  # Left stick up/down
+        self.axis_rotate = 2  # CHANGE LATER ON
+        #self.axis_angular = 6  # Left stick left/right
+        self.max_linear = 0.1 # maximum linear velocity
+        self.max_angular = 0.05 # maximum angular velocity
 
-    def joy_callback(self, msg):
-        # Read joystick values
-        linear = msg.axes[self.axis_linear] * self.scale_linear  # Forward/backward
-        angular = msg.axes[self.axis_angular] * self.scale_angular  # Left/right turn
+        # Robot parameters
+        self.wheel_radius = 0.04921  # meters
+        self.wheel_base = 0.3125  # meters
 
-        # Convert to motor duty cycles
-        left_duty = linear + angular
-        right_duty = linear - angular
+    def twist2duty_callback(self, msg):
+        # Convert from cmd_vel to motor duty cycles
+        vx = msg.linear.x
+        vy = msg.linear.y
+        w = msg.angular.z 
 
-        # Normalize values between -1 and 1
-        left_duty = max(min(left_duty, 1.0), -1.0)
-        right_duty = max(min(right_duty, 1.0), -1.0)
+        # if we are using the controller
+        if w == 0:
+            left_duty = vx/self.max_linear
+            right_duty = vx/self.max_linear
+        else:
+            left_duty = w /self.max_angular 
+            right_duty = - w /self.max_angular
 
         print('left duty ', left_duty)
         print('right duty ', right_duty)
 
         # Publish motor commands
-        #duty_msg = Float32MultiArray()
-        #duty_msg.data = [left_duty, right_duty]
-        msg.header = Header()
-        msg.header.stamp = self.get_clock().now().to_msg()
+        duty_msg = DutyCycles() # Init
+        duty_msg.header = Header()
+        duty_msg.header.stamp = self.get_clock().now().to_msg()
 
-        msg = DutyCycles() # Init
-        msg.duty_cycle_left = left_duty
-        msg.duty_cycle_right = right_duty
-        self.publisher.publish(msg)
+        duty_msg.duty_cycle_left = left_duty
+        duty_msg.duty_cycle_right = right_duty
+        self.publisher.publish(duty_msg)
 
 def main(args=None):
     rclpy.init(args=args)
-    node = F710Teleop()
+    node = Twist2Duty()
     rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
