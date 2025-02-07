bf9e482e1696e40f7e5d5bb30571e2af5ad19e5a
pcsk <pcsk@kth.se>
2025-02-07 16:47:55 +0100

    path_planner code v1

--- robp_ws/src/path_planner/path_planner/path_planner.py
+++ robp_ws/src/path_planner/path_planner/path_planner.py
@@ -3,36 +3,60 @@
 import rclpy
 import math
 from rclpy.node import Node
-
-def normalize_angle(angle):
-    """Normalize angle to the range [-180, 180] degrees."""
-    while angle > 180:
-        angle -= 360
-    while angle < -180:
-        angle += 360
-    return angle
-
+from geometry_msgs.msg import Point, Twist
+from nav_msgs.msg import Odometry
+from std_msgs.msg import Bool
+from geometry_msgs.msg import PoseStamped
+import tf_transformations
 
 class path_planner(Node):
     def __init__(self):
         super().__init__('carrot_planner')
-        self.cur_pos = self.create_subscription(PoseStamped, '/path', self.odometry_callback, 10)
-        self.goal = self.create_subscription(PoseStamped, '/goal_pose', self.goal_callback, 10)
-        self.goal_reached = self.create_publisher(Bool, '/goal_reached', 10) #this should be a boolean
+
+        self.goal = Point()
+        self.goal.x = 0.0
+        self.goal.y = 0.0
+        self.is_goal = True
+        
+        self.create_subscription(PoseStamped, '/path', self.odometry_callback, 10)
+        self.create_subscription(Point, '/goal', self.goal_callback, 10)
+        self.goal_reached = self.create_publisher(Bool, '/goal_reached', 10)
         self.publisher = self.create_publisher(Twist, '/cmd_vel', 10)
 
-    def odometry_callback(self):
-        # check if we reached the goal position and if so, we publish /goal/reached message
-        return
+    def odometry_callback(self, msg: PoseStamped):
+        if self.is_goal == False:
+            theta_g = math.atan2(self.goal.y - msg.position.y, 
+                                 self.goal.x - msg.position.x)
+            qz = msg.orientation.z
 
-    def goal_callback(self):
-        # run once we received a new goal
+            tf_transformations.euler_from_quaternion([0, 0, qz, 1])
 
-        # first rotate robot to face the correct position
-        theta_g = math.degrees(math.atan2(self.goal.position.y - self.cur_pos.position.y, self.goal.position.x - self.cur_pos.position.x))
-        delta_theta = normalize_angle(theta_g - self.cur_pos.orientation.z)
+            delta_theta = theta_g - theta_r
+            if delta_theta >= 0.01:
+                rotate = Twist()
+                rotate.angular.z = 0.005 # TODO check angular vel
+                self.publisher.publish(rotate)
+            else:
+                rotate = Twist()
+                rotate.angular.z = 0.0
+                self.publisher.publish(rotate)
 
-        # then give a linear vel command to move the robot
+                h = math.sqrt((self.goal.x - msg.position.x) ** 2 + 
+                              (self.goal.y - msg.position.y) ** 2)
+                if h >= 0.1:
+                    move = Twist()
+                    move.linear.x = 0.01
+                    self.publisher.publish(move)
+                else:
+                    move = Twist()
+                    move.linear.x = 0.0
+                    self.publisher.publish(move)
+                    self.goal_reached.publish(Bool(data=True))
+                    self.is_goal = True
 
-        # when the robot reaches its goal, the odometry_callback will detect it and stop it.
+    def goal_callback(self, msg: Point):
+        self.goal.x = msg.x
+        self.goal.y = msg.y
+        self.goal_reached.publish(Bool(data=False))
+        self.is_goal = False
         return
