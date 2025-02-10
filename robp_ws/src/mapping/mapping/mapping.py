055899998feb6dd7fcf192530ba0fcd8eda5b862
Sebastian Christensson <schris@kth.se>
2025-02-10 16:08:32 +0100

    N =100

--- robp_ws/src/mapping/mapping/mapping.py
+++ robp_ws/src/mapping/mapping/mapping.py
@@ -26,7 +26,7 @@ class Mapping(Node):
         # Initializes the projection 
         self.proj = LaserProjection() # Converts a LaserScan to a PointCloud2
 
-        self.N = 10 # Evert N:th time 
+        self.N = 100 # Evert N:th time 
         self.counter = 0
 
     def listener_callback(self, msg):
