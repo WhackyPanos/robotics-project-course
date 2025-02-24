6b0ec6eb2e231b1bd29b82f4ec78db8ec8d6bf48
Dominik Friedrich <domfri@kth.se>
2025-02-24 14:33:50 +0100

    nothing more

--- robp_ws/src/robp_robot/robp_launch/launch/pick_objects_launch.py
+++ robp_ws/src/robp_robot/robp_launch/launch/pick_objects_launch.py
@@ -1,10 +1,37 @@
+from launch_ros.actions import Node
 import launch
 from launch_ros.actions import Node
+import os
 from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource, AnyLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
-
+    robp_launch_dir = get_package_share_directory('robp_launch')
     return launch.LaunchDescription([
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(os.path.join(robp_launch_dir, 'launch/phidgets_launch.py'))
+        ),
+
+        IncludeLaunchDescription(
+            AnyLaunchDescriptionSource(os.path.join(robp_launch_dir, 'launch/frames_launch.xml'))
+        ),
+
+        IncludeLaunchDescription(
+            AnyLaunchDescriptionSource(os.path.join(robp_launch_dir, 'launch/lidar_launch.yaml'))
+        ),
+
+        Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            arguments=['--frame-id', 'map', '--child-frame-id', 'odom']
+        ),
+        Node(
+            package='odometry',
+            executable='odometry'
+        ),
+
         Node(
             package='behavior_tree',
             executable='pick_bt',
@@ -12,3 +39,5 @@ def generate_launch_description():
             emulate_tty=True   # Ensures that print() statements are immediately flushed
         )
     ])
+
+
