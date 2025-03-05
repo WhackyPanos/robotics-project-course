f9bdeb27ac9b4461b06fecabd295c19dcf153719
Dominik Friedrich <domfri@kth.se>
2025-03-05 13:09:27 +0100

    added motion node, which is a remake of the 'path_planner' node with PID control.

--- robp_ws/src/path_planner/setup.py
+++ robp_ws/src/path_planner/setup.py
@@ -21,6 +21,7 @@ setup(
     entry_points={
         'console_scripts': [
             'path_planner = path_planner.path_planner:main',
+            'motion = path_planner.motion:main',
             'point_generator = path_planner.point_generator:main',
             'point_publisher_test = path_planner.point_publisher_test:main',
             'path_execution = path_planner.path_execution:main'
