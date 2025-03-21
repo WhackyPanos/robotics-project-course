5a7a706a63d7e6fd575ab983e5bb39e81e1656ee
Dominik Friedrich <domfri@kth.se>
2025-03-21 19:22:19 +0100

    merge conflicts

--- robp_ws/src/robp_robot/robp_launch/launch/exploration_hw_tf_launch.py
+++ robp_ws/src/robp_robot/robp_launch/launch/exploration_hw_tf_launch.py
@@ -22,8 +22,8 @@ def generate_launch_description():
             AnyLaunchDescriptionSource(os.path.join(robp_launch_dir, 'launch/frames_launch.xml'))
         ),
 
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(os.path.join(robp_launch_dir, 'launch/phidgets_launch.py'))
-        ),
+        # IncludeLaunchDescription(
+        #     PythonLaunchDescriptionSource(os.path.join(robp_launch_dir, 'launch/phidgets_launch.py'))
+        # ),
 
     ])
